a1f4b8ded54fba812e6196e6e9def1e906a01c1e
Gonza <gonl@gej.com>
2023-07-18 17:50:29 +0200

    style: Incorpora diseño de las tareas.

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -9,6 +9,7 @@ function ListaDeTareas() {
     const [tareas, setTareas] = useState([]);
 
     const agregarTarea = tarea => {
+        console.log(tarea);
         if (tarea.texto.trim()) {
             tarea.texto = tarea.texto.trim();
             const tareasActualizadas = [tarea, ...tareas];
