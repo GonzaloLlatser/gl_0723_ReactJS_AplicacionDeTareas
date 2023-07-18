c36d0b1325f06e6258deef0531f1be19861b3804
Gonza <gonl@gej.com>
2023-07-18 16:43:46 +0200

    feat: Especifica los estados de la lista de tareas, muestra la lista y genera funciones completar y eliminar.

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import TareaFormulario from "./TareaFormulario";
 import '../hojas-de-estilo/ListaDeTareas.css'
 
 function ListaDeTareas() {
+
+    cosnt[tareas, setTareas] = useState([]);
+
     return (
         <>
             <TareaFormulario />
             <div className='tareas-lista-contenedor'>
-                LISTA DE TAREAS
+                {
+                    tareas.map((tarea) =>
+                        <Tarea
+                            texto={tarea.texto}
+                            completada={tarea.completada}
+                        />
+                    )
+                }
             </div>
         </>
     );
